da19843b457073dd3af10cfdc1af41a288df8d08
Your Name <you@example.com>
2015-06-10 10:54:11 +0800

    add pure

--- music_pro/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class MusicSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(source='*',read_only=True)
-
-    class Meta:
-        model = Music
-        fields = ('username','music_file','music_img','user')
